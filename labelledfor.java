ee765d57e710c736e2dc7b37470aafb7171d53c0
sambhavgarrg <sambhavgarrg@gmail.com>
2017-12-25 04:54:32 +0530

    new

---
+++ labelledfor.java
@@ -0,0 +1,17 @@
+public class labelledfor{
+ public static void main(String []args)
+ {
+  int i,j;
+  aa:
+  for(i=0;i<=3;i++)
+  {
+    bb:
+    for(j=0;j<=3;j++)
+    {
+      if(i==2 || j==2)
+      break bb;
+      System.out.println("aa: "+i+"bb: "+j);
+    }
+  }
+ }
+}
---
+++ refinitobj.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class refinitobj{
+  int grade;
+  String name;//instance variables
+ public static void main(String args[])//main can be outside the class in another class too
+ {
+   Scanner scan=new Scanner(System.in);
+   Test t1=new Test();
+   Test t2=new Test();
+   System.out.print("Enter name: ");
+   t1.name=scan.next();
+   System.out.print("Enter grade: ");
+   t1.grade=scan.nextInt();
+   System.out.print("Enter name: ");
+   t2.name=scan.next();
+   System.out.print("Enter grade: ");
+   t2.grade=scan.nextInt();
+   System.out.print("\nname: "+t1.name+"\ngrade: "+t1.grade+"\n");//initializing object through reference
+   System.out.print("\nname: "+t2.name+"\ngrade: "+t2.grade+"\n");//initializing object through reference
+ }
+};
