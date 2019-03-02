1a6a98259949d2c6c97dac7660880f38eea92aef
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-03-02 22:01:46 +0530

    2/3/19 update

--- Java/src/packagize/Test.java
+++ Java/src/packagize/Test.java
@@ -3,9 +3,10 @@ import bin.A;
 import bin.B;
 
 class Test{
-  public static void main(){
+  public static void main(String[] args){
     A obj = new A();
     B obj1 = new B();
     System.out.println("Hello from Test!");
   }
 }
+// java p3.Test
