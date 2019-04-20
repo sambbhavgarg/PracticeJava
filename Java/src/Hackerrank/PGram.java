4883fe3a2f96939e722350137ea522bd3c510469
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-04-20 13:15:34 +0530

    4.20.19

--- Java/src/Hackerrank/PGram.java
+++ Java/src/Hackerrank/PGram.java
@@ -5,30 +5,33 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-class OutOfHereException extends Exception{
-   OutOfHereException(String s){
-   }
-}
+// class OutOfHereException extends Exception{
+//    OutOfHereException(String s){
+//      super(s);
+//    }
+// }
 
 public class PGram{
   static int B, H;
+  static boolean flag;
   static{
     Scanner scan = new Scanner(System.in);
     B = scan.nextInt();
     H = scan.nextInt();
-    if (B<=100 && H<=100 && B>0 && H>0)
-      throw OutOfHereException("Breadth and height must be positive.");
-  }
-
-
-  public static void main(String[] args){
+    flag = true;
     try{
-      int area = B*H;
-  		System.out.print(area);
+      if (B>=100 || B<0 || H>=100 || H<0){
+        flag = false;
+        throw new Exception("Breadth and height must be positive");
+      }
+    }catch(Exception e){
+      System.out.println(e);
     }
-    catch(OutOfHereException I){
-      System.out.println("java.lang.Exception: ");
-    }
-
+  }
+  public static void main(String[] args){
+      if(flag){
+        int area = B*H;
+    		System.out.println(area);
+      }
   }//end of main
 }//end of class
--- Java/src/Hackerrank/Test.java
+++ Java/src/Hackerrank/Test.java
@@ -1,10 +1,22 @@
 package bin.hackerrank;
+import java.util.*;
 
 class Test{
   public static void main(String[] args) {
-    int n = -100;
-    if(n <=  -100 && n>= -100){
-      System.out.println("hello");
-    }
+    // int n = -100;
+    // if(n <=  -100 && n>= -100){
+    //   System.out.println("hello");
+    // }
+    //-------------------------------------
+    // Scanner in = new Scanner(System.in);
+    // int n = in.nextInt();
+    // in.close();
+    // //String s=???; Complete this line below
+    // Integer i = new Integer(n);
+    // String s = i.toString();
+    // System.out.println(s+i);
+    //-------------------------------------
+    Calendar c = Calendar.getInstance();
+    System.out.println("The Current Date is:" + c.getTime());
   }
 }
