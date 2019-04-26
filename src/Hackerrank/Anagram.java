b1ee209df71f71cb7e5ed4a972db163aee719ceb
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-04-26 23:18:37 +0530

    4.26.19

--- src/Hackerrank/Anagram.java
+++ src/Hackerrank/Anagram.java
@@ -1,7 +1,7 @@
 package bin.hackerrank;
 import java.util.Scanner;
 
-public class Solution {
+public class Anagram {
 
     static boolean isAnagram(String a, String b) {
       int [] x = new int[26];
@@ -12,18 +12,23 @@ public class Solution {
            int posa = (int) a.charAt(i);
            int posb = (int) b.charAt(j);
 
-           if(pos>90){
+           if(posa>=97 && posb>=97){
             ++x[posa-97];
             ++y[posb-97];
            }
 
-           else{
+           else if(posa>=65 && posb>=65){
             ++x[posa-65];
-            ++y[posb-97];
+            ++y[posb-65];
           }
         }
+        int flag = 0;
+        for(int i=0; i<x.length-1; i++)
+          if(x[i]==y[i]) ++flag;
+        if (flag == 26) return true;
       }
-      else return false;
+      else
+        return false;
     }
 
     public static void main(String[] args) {
