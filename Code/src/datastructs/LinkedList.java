30af9916debc3acdf2c941f756b57322a27cc7a2
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-05-19 20:14:14 +0530

    5.19.19

--- Code/src/datastructs/LinkedList.java
+++ Code/src/datastructs/LinkedList.java
@@ -1,4 +1,4 @@
-package datastructs;
+ package datastructs;
 
 public class LinkedList {
 	Node head;
@@ -14,19 +14,55 @@ public class LinkedList {
 
 	void push(int new_data){
 		Node new_node = new Node(new_data);
+		new_node.next = head;
 		head = new_node;
-		new_node = head;
 	}
 
-	void insertAfter(Node prev_node, int new_data){
-		if(prev_node == null){
-			System.out.println("Previous node can't be null");
+	void insertBefore(int value, int new_data){
+		if(head == null){
+			System.out.println("Head can't be null");
 			return;
 		}
-
+		
 		Node new_node = new Node(new_data);
-		new_node.next = prev_node.next;
-		prev_node.next = new_node;
+
+		Node temp = head;
+		System.out.println("Outside while");
+		while(temp.next.next != null) {
+			temp = temp.next;
+			System.out.println("Inside while");
+			if(temp.next.data == value) {
+				new_node.next = temp.next;
+				temp.next = new_node;
+				return;
+			}
+			else {
+				System.out.println("Entered Node does not exist");
+			}
+		}
+	}
+	
+	void insertAfter(int value, int new_data){
+		if(head == null){
+			System.out.println("Head can't be null");
+			return;
+		}
+		
+		Node new_node = new Node(new_data);
+
+		Node temp = head;
+		System.out.println("Outside while");
+		int flag = 0;
+		while(temp.next.next != null) {
+			temp = temp.next;
+//			System.out.println("Inside while");
+			if(temp.next.data == value) {
+				new_node.next = temp.next;
+				temp.next = new_node;
+				return;
+			}
+			flag++;
+		}
 	}
 
 	void insertEnd(int new_data){
@@ -48,20 +84,29 @@ public class LinkedList {
 
 	void printList(){
 		Node temp = head;
-		if(head == null){
-			System.out.println("List Empty.");
-			return;
-		}
-		while(temp.next!=null){
-			System.out.println("list: "+temp.data+" ");
+		// if(head == null){
+		// 	System.out.println("List Empty.");
+		// 	return;
+		// }
+		System.out.print("list: ");
+		while(temp!=null){
+			System.out.print(temp.data+" ");
 			temp = temp.next;
 		}
 	}
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-
 		list.push(5);
+		list.push(6);
+		list.push(54);
+		list.push(90);
+		list.push(56);
+		list.push(34);
+		list.push(50);
+		list.insertAfter(5, 20);
+		list.insertEnd(67);
+		list.push(6);
 		list.printList();
 	}
 }
