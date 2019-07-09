6719de84f968b3f24c664fef0b7e912d0a0db962
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-07-09 23:03:53 +0530

    used Predicate class in java.util.function

--- Code/src/lambda-expressions/PalindromeTest.java
+++ Code/src/lambda-expressions/PalindromeTest.java
@@ -3,10 +3,11 @@ import java.lang.String;
 import java.lang.StringBuilder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;//Adding this makes the interface obsolete
 
-interface StrPredicate{
-  boolean test(String str);
-}
+// interface StrPredicate{
+//   boolean test(String str);
+// }
 
 public class PalindromeTest{
   public static void main(String[] args) {
@@ -19,12 +20,12 @@ public class PalindromeTest{
     words.add("Java");
     System.out.println("List of words: "+words);
 
-    StrPredicate predicate1 = (String str)->
+    Predicate<String> predicate1 = (String str)->
             str.equals(new StringBuilder(str).reverse().toString());
     List<String> cspalindromes = filterString(words, predicate1);
     System.out.println("Case Sensitive Palindromes: " + cspalindromes);
 
-    StrPredicate predicate2 = (String str)->
+    Predicate<String> predicate2 = (String str)->
             str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
     List<String> cispalindromes = filterString(words, predicate2);
     System.out.println("Case Sensitive Palindromes: " + cispalindromes);
@@ -35,7 +36,7 @@ public class PalindromeTest{
   * @param predicate Provides criteria for filtering the string
   * @return List of strings that match the criteria
   */
-  public static List<String> filterString(List<String> strList, StrPredicate predicate){
+  public static List<String> filterString(List<String> strList, Predicate<String> predicate){
     List<String> result = new ArrayList<>();
     for(String str: strList)
       if(predicate.test(str))
